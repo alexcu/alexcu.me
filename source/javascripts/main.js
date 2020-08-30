732c16739d4b66bc0e69c224b1397308583977b4
Alex Cummaudo <alexcu@me.com>
2020-08-30 11:03:19 +1000

    Fix broken JavaScript and remove old code

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -18,80 +18,10 @@ function setupSwitchText (headerTagline, linkList) {
     };
   };
 }
-// Now setup
-setupSwitchText(  document.querySelector("body > header > h2"),
-                  document.querySelector("body > header > ul"));
-
-setupSwitchText(  document.querySelector("section#technologies > header  > h2"),
-                  document.querySelector("section#technologies > article > ul"));
-/**
- * Alters a link for touch enabled devices
- */
-var tappedEl;
-function touchFriendly (anchorEl) {
-  var isTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
-  // Ignore for non-touch
-  if (!isTouch) {
-    return;
-  }
-  firstTap = function (e) {
-    tappedEl = e.target; // currently tapped element
-    // next time it is tapped, secondTap
-    e.target.onclick = secondTap;
-    (e.preventDefault ? e.preventDefault() : function() { e.returnValue = false; })()
-  }
-  secondTap = function (e) {
-    // only commit this tap if it's the tappedEl or its descendant
-    var targetIsTappedEl = tappedEl == e.target;
-    var targetIsDescendantOfTappedEl = false;
-    for (i in els = tappedEl.querySelectorAll("*")) {
-      if (els[i] == e.target) {
-        targetIsDescendantOfTappedEl = true;
-        break;
-      }
-    }
-    if (targetIsTappedEl || targetIsDescendantOfTappedEl) {
-      // next time it is tapped, firstTap
-      e.target.onclick = firstTap;
-      e.returnValue = true;
-    }
-    // Else cancel
-    else {
-      tappedEl.onclick = tappedEl.onclick = firstTap;
-      (e.preventDefault ? e.preventDefault() : function() { e.returnValue = false; })()
-    }
-
-  }
-  anchorEl.onclick = firstTap;
-}
-var anchorEls = document.querySelectorAll('a[href^="http"]');
-for (var i = 0; i < anchorEls.length; i++) {
-  //touchFriendly(anchorEls[i]);
-};
-/**
- * Sets up the modal
- */
-function setupModal(modalEl) {
-  // Add modal class to body
-  var srcAnchor = document.querySelector('a[href="#' + modalEl.id + '"]');
-  if (srcAnchor == null) {
-    return;
-  }
-  srcAnchor.onclick = function() {
-    document.querySelector('body').classList.add('modal');
-    modalEl.querySelector("ul").scrollTop = 0;
-  }
-  // Setup close modal
-  var closeEl = modalEl.querySelector('button.close');
-  closeEl.onclick = function () {
-    document.querySelector('body').classList.remove('modal');
-    window.location.hash = 'modal-' + modalEl.id + '-src';
-  };
-}
-var modalEls = document.querySelectorAll('dialog.modal');
-for (var i = 0; i < modalEls.length; i++) {
-  setupModal(modalEls[i]);
-};
+setupSwitchText(
+  document.querySelector("body > header > h2"),
+  document.querySelector("body > header > ul")
+);
 /**
  * Updates value CSS input for form validation
  */
@@ -151,42 +81,6 @@ form.onsubmit = function () {
   xhr.send(formData);
   return false;
 };
-/**
- * Percent update on modal gallery scroll
- */
-function setupGallery(modal) {
-  var el = modal.querySelector("ul");
-  el.onscroll = function () {
-    var percent = el.scrollTop / (el.scrollHeight - el.clientHeight + 1);
-    // Update the progress
-    var progressEl = modal.querySelector("progress");
-    progressEl.value = percent;
-    // How much one image is in pct (#children - last element)
-    if (percent > 0) {
-      modal.querySelector("header").classList.add("expanded");
-    } else {
-      modal.querySelector("header").classList.remove("expanded");
-    }
-  }
-  // Portrait images add class to images
-  var imgs = el.querySelectorAll("img");
-  for (var i = 0; i < imgs.length; i++) {
-    if (imgs[i].width < imgs[i].height) {
-      imgs[i].classList.add("portrait");
-    }
-  };
-}
-var galleryModalEls = document.querySelectorAll('dialog.modal.gallery');
-for (var i = 0; i < galleryModalEls.length; i++) {
-  setupGallery(galleryModalEls[i]);
-};
-/**
- * Remove modal from hash if loaded in with modal
- */
-if (window.location.hash.indexOf("modal") > 0) {
-  window.location.hash = "";
-  document.querySelector('body').classList.remove('modal');
-}
 /**
  * All external hyperlinks open in blank target
  */
